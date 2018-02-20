4b92e2a0f79eefc5ae160c6c7ab68a9463ceb04a
Rangi <remy.oukaour+rangi42@gmail.com>
2018-02-20 11:47:53 -0500

    New Cinnabar Lab tiles

--- data/tilesets/pallet_cinnabar_collision.asm
+++ data/tilesets/pallet_cinnabar_collision.asm
@@ -196,29 +196,33 @@
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR
 	tilecoll FLOOR, FLOOR, WALL, WALL
 	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR
-	tilecoll WALL, WALL, WALL, DOOR
-	tilecoll WALL, WALL, WALL, WALL
 	tilecoll WALL, TOP_WALL, WALL, FLOOR
 	tilecoll TOP_WALL, WALL, FLOOR, WALL
-	tilecoll TOP_WALL, TOP_WALL, WALL, WALL
-	tilecoll TOP_WALL, TOP_WALL, WALL, WALL
-	tilecoll TOP_WALL, TOP_WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, DOOR
+	tilecoll WALL, WALL, WALL, WALL
 	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll WALL, WALL, FLOOR, WALL
-	tilecoll WALL, WALL, DOOR, WALL
-	tilecoll WALL, WALL, FLOOR, FLOOR
+	tilecoll FLOOR, FLOOR, WALL, WALL
+	tilecoll TOP_WALL, TOP_WALL, WALL, WALL
+	tilecoll TOP_WALL, TOP_WALL, WALL, WALL
+	tilecoll TOP_WALL, TOP_WALL, WALL, WALL
 	tilecoll FLOOR, WALL, FLOOR, WALL
-	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll WALL, WALL, WALL, WALL
-	tilecoll WALL, WALL, WALL, WALL
-	tilecoll WALL, WALL, WALL, WALL
-	tilecoll WALL, FLOOR, WALL, FLOOR
 	tilecoll WALL, WALL, FLOOR, WALL
 	tilecoll WALL, WALL, DOOR, WALL
 	tilecoll WALL, WALL, FLOOR, FLOOR
+	tilecoll WALL, FLOOR, WALL, FLOOR
+	tilecoll FLOOR, FLOOR, WALL, WALL
+	tilecoll FLOOR, FLOOR, WALL, WALL
+	tilecoll FLOOR, FLOOR, WALL, WALL
 	tilecoll FLOOR, WALL, FLOOR, FLOOR
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll FLOOR, FLOOR, WALL, FLOOR
+	tilecoll WALL, WALL, FLOOR, WALL
+	tilecoll WALL, WALL, DOOR, WALL
+	tilecoll WALL, WALL, FLOOR, FLOOR
+	tilecoll TV, TV, TV, TV
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, DOOR, WALL
