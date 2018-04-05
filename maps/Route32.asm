d73ac933863d5d2579beee44e3e64217208f8b09
Rangi <remy.oukaour+rangi42@gmail.com>
2018-04-05 12:40:44 -0400

    Tweak Violet-Route 32 gate

--- maps/Route32.asm
+++ maps/Route32.asm
@@ -8,7 +8,7 @@ Route32_MapScriptHeader:
 	warp_event  8,  6, ROUTE_32_RUINS_OF_ALPH_GATE, 3
 	warp_event  8,  7, ROUTE_32_RUINS_OF_ALPH_GATE, 4
 	warp_event 10, 83, UNION_CAVE_1F, 4
-	warp_event 16,  1, VIOLET_CITY, 12
+	warp_event 17,  1, VIOLET_CITY, 12
 	warp_event  9, 29, HIDDEN_TREE_GROTTO, 1
 
 	db 0 ; coord events
