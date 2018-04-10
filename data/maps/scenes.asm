acdd017dc451c4d10aa6fe2a734ee034501f6f47
TheFakeMateo <skeetendomateo@gmail.com>
2018-04-10 14:31:25 -0500

    Viridian Mart Oaks Parcel Event

--- data/maps/scenes.asm
+++ data/maps/scenes.asm
@@ -8,6 +8,7 @@ ENDM
 MapTriggers::
 	scene_var PALLET_TOWN, wPalletTownTrigger
 	scene_var OAKS_LAB, wOaksLabTrigger
+	scene_var VIRIDIAN_MART, wViridianMartTrigger
 	scene_var ROUTE_18_WEST, wAlways0Trigger
 	scene_var ROUTE_24, wRoute24Trigger
 	db -1
