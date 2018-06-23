8d14d1287e349b25ee21feeb2f848e1c3a5b3cae
TheFakeMateo <skeetendomateo@gmail.com>
2018-06-23 16:45:02 -0500

    Viridian Spearow House events

--- maps/ViridianNicknameSpeechHouse.asm
+++ maps/ViridianNicknameSpeechHouse.asm
@@ -11,6 +11,32 @@ ViridianNicknameSpeechHouse_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 4 ; object events
+	object_event  5,  3, SPRITE_BALDING_GUY, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, ViridianHouseText1, -1
+	object_event  1,  4, SPRITE_LITTLE_GIRL, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, ViridianHouseText2, -1
+	object_event  5,  5, SPRITE_BIRD, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, PERSONTYPE_POKEMON, SPEAROW, ViridianHouseText3, -1
+	object_event  4,  0, SPRITE_BOOK_PAPER_POKEDEX, SPRITEMOVEDATA_TILE_UP, 0, 0, -1, -1, 0, PERSONTYPE_COMMAND, jumptext, ViridianHouseText4, -1
 
-	const_def 1 ; object constants
+ViridianHouseText1:
+	text "Coming up with"
+	line "nicknames is fun,"
+	cont "but hard."
+
+	para "Simple names are"
+	line "the easiest to"
+	cont "remember."
+	done
+
+ViridianHouseText2:
+	text "My Daddy loves"
+	line "#mon too."
+	done
+
+ViridianHouseText3:
+	text "Speary: Tetweet!"
+	done
+
+ViridianHouseText4:
+	text "Spearow"
+	line "Name: Speary"
+	done
