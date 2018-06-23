d83e752b012d1700142b94c68d8b7f0dbf2080b2
TheFakeMateo <skeetendomateo@gmail.com>
2018-06-23 05:06:52 -0500

    Begin restoring WildMon data

--- data/wild/kanto_grass.asm
+++ data/wild/kanto_grass.asm
@@ -1,2 +1,136 @@
+	map_id ROUTE_1
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 3, PIDGEY
+	db 3, RATTATA
+	db 3, RATTATA
+	db 2, RATTATA
+	db 2, PIDGEY
+	db 4, PIDGEY
+	db 5, RATTATA
+	; day
+	db 3, PIDGEY
+	db 3, RATTATA
+	db 3, RATTATA
+	db 2, RATTATA
+	db 2, PIDGEY
+	db 4, PIDGEY
+	db 5, RATTATA
+	; nite
+	db 3, HOOTHOOT
+	db 3, RATTATA
+	db 3, RATTATA
+	db 2, RATTATA
+	db 2, HOOTHOOT
+	db 4, HOOTHOOT
+	db 5, RATTATA
+
+	map_id ROUTE_22
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 2, NIDORAN_F
+	db 2, NIDORAN_M
+	db 3, MANKEY
+	db 3, MEOWTH
+	db 4, MANKEY
+	db 4, SPEAROW
+	db 6, SPEAROW
+	; day
+	db 2, NIDORAN_F
+	db 2, NIDORAN_M
+	db 3, MANKEY
+	db 3, MEOWTH
+	db 4, MANKEY
+	db 4, SPEAROW
+	db 6, SPEAROW
+	; nite
+	db 2, NIDORAN_F
+	db 2, NIDORAN_M
+	db 3, MANKEY
+	db 3, MEOWTH
+	db 4, MEOWTH
+	db 4, SPEAROW
+	db 6, SPEAROW
+
+	map_id ROUTE_2_SOUTH
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 3, RATTATA
+	db 3, PIDGEY
+	db 5, NIDORAN_M
+	db 5, NIDORAN_F
+	db 3, ZIGZAGOON
+	db 5, ZIGZAGOON
+	db 5, RATTATA
+	; day
+	db 3, RATTATA
+	db 3, PIDGEY
+	db 5, NIDORAN_M
+	db 5, NIDORAN_F
+	db 3, ZIGZAGOON
+	db 5, ZIGZAGOON
+	db 5, RATTATA
+	; nite
+	db 3, RATTATA
+	db 3, HOOTHOOT
+	db 5, NIDORAN_M
+	db 5, NIDORAN_F
+	db 3, ZIGZAGOON
+	db 5, ZIGZAGOON
+	db 5, RATTATA
+
+	map_id VIRIDIAN_FOREST
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 4, WEEDLE
+	db 4, CATERPIE
+	db 5, KAKUNA
+	db 5, METAPOD
+	db 3, ZIGZAGOON
+	db 3, PIKACHU
+	db 5, PIKACHU
+	; day
+	db 4, WEEDLE
+	db 4, CATERPIE
+	db 5, KAKUNA
+	db 5, METAPOD
+	db 3, ZIGZAGOON
+	db 3, PIKACHU
+	db 5, PIKACHU
+	; nite
+	db 4, WEEDLE
+	db 4, CATERPIE
+	db 5, KAKUNA
+	db 5, METAPOD
+	db 3, ZIGZAGOON
+	db 3, PIKACHU
+	db 5, PIKACHU
+
+	map_id ROUTE_2_NORTH
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	; morn
+	db 4, RATTATA
+	db 4, PIDGEY
+	db 6, NIDORAN_M
+	db 6, NIDORAN_F
+	db 4, ZIGZAGOON
+	db 6, ZIGZAGOON
+	db 6, RATTATA
+	; day
+	db 4, RATTATA
+	db 4, PIDGEY
+	db 6, NIDORAN_M
+	db 6, NIDORAN_F
+	db 4, ZIGZAGOON
+	db 6, ZIGZAGOON
+	db 6, RATTATA
+	; nite
+	db 4, RATTATA
+	db 4, HOOTHOOT
+	db 6, NIDORAN_M
+	db 6, NIDORAN_F
+	db 4, ZIGZAGOON
+	db 6, ZIGZAGOON
+	db 6, RATTATA
 
 	db -1 ; end
