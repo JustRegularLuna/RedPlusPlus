936d39fad2ec9d3bf695039a4b4d51b93847924e
Rangi <remy.oukaour+rangi42@gmail.com>
2018-06-30 23:40:38 -0400

    Map Ilex Forest (to do: attributes+collisions)

--- maps/IlexForest.asm
+++ maps/IlexForest.asm
@@ -4,14 +4,15 @@ IlexForest_MapScriptHeader:
 	db 0 ; callbacks
 
 	db 3 ; warp events
-	warp_event  3,  7, ROUTE_34_ILEX_FOREST_GATE, 3
-	warp_event  5, 44, ILEX_FOREST_AZALEA_GATE, 1
-	warp_event  5, 45, ILEX_FOREST_AZALEA_GATE, 2
+	warp_event  6,  9, ROUTE_34_ILEX_FOREST_GATE, 3
+	warp_event  7, 46, ILEX_FOREST_AZALEA_GATE, 1
+	warp_event  7, 47, ILEX_FOREST_AZALEA_GATE, 2
 
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	cuttree_event 12, 31, -1
 
 	const_def 1 ; object constants
