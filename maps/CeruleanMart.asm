2c0cdf3388729c841be57ac2b502da47a6e44ca1
TheFakeMateo <skeetendomateo@gmail.com>
2019-02-05 16:54:47 -0600

    Cerulean Mart and Pokecenter events

--- maps/CeruleanMart.asm
+++ maps/CeruleanMart.asm
@@ -11,6 +11,27 @@ CeruleanMart_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 3 ; object events
+	mart_clerk_event  1,  3, MARTTYPE_STANDARD, MART_CERULEAN
+	object_event  7,  6, SPRITE_BLACK_HAIR_BOY_2, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, CeruleanMartText2, -1
+	object_event  6,  2, SPRITE_LASS, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, CeruleanMartText3, -1
 
-	const_def 1 ; object constants
+CeruleanMartText2:
+	text "Use Repel to keep"
+	line "bugs and weak"
+	cont "#mon away."
+
+	para "Put your strongest"
+	line "#mon at the"
+	cont "top of the list"
+	cont "for best results!"
+	done
+
+CeruleanMartText3:
+	text "Have you seen any"
+	line "Rare Candy?"
+
+	para "It's supposed to"
+	line "make #mon go"
+	cont "up one level!"
+	done
--- maps/CeruleanPokeCenter1F.asm
+++ maps/CeruleanPokeCenter1F.asm
@@ -12,6 +12,33 @@ CeruleanPokeCenter1F_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 4 ; object events
+	pc_nurse_event  5,  1
+	pc_chansey_event  6,  1
+	object_event 10,  5, SPRITE_BLACK_HAIR_BOY_2, SPRITEMOVEDATA_WANDER, 0, 0, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, CeruleanPokecenterText2, -1
+	object_event  4,  3, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, CeruleanPokecenterText3, -1
 
-	const_def 1 ; object constants
+CeruleanPokecenterText2:
+	text "That Bill!"
+
+	para "I heard that"
+	line "he'll do whatever"
+	cont "it takes to get"
+	cont "rare #mon!"
+	done
+
+CeruleanPokecenterText3:
+	text "Have you heard"
+	line "about Bill?"
+
+	para "Everyone calls him"
+	line "a # Maniac!"
+
+	para "I think people"
+	line "are just jealous"
+	cont "of Bill, though."
+
+	para "Who wouldn't want"
+	line "to boast about"
+	cont "their #mon?"
+	done
