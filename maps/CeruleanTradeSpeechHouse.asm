6c9bfafba6c1449888b8e33cd61cc1ab3ada4bd6
TheFakeMateo <skeetendomateo@gmail.com>
2019-04-11 15:09:01 -0500

    Cerulean City Trade House events

--- maps/CeruleanTradeSpeechHouse.asm
+++ maps/CeruleanTradeSpeechHouse.asm
@@ -11,6 +11,16 @@ CeruleanTradeSpeechHouse_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 2 ; object events
+	object_event  5,  4, SPRITE_OLD_MEDIUM_WOMAN, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, CeruleanHouse1Text1, -1
+	object_event  1,  2, SPRITE_GAMBLER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, PERSONTYPE_COMMAND, trade, TRADE_WITH_RALPH_FOR_JYNX, -1
 
-	const_def 1 ; object constants
+CeruleanHouse1Text1:
+	text "My husband likes"
+	line "trading #mon."
+
+	para "If you are a"
+	line "collector, would"
+	cont "you please trade"
+	cont "with him?"
+	done
