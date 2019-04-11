230fa1468fbf57209f9effb66cc23dea81857fc9
TheFakeMateo <skeetendomateo@gmail.com>
2019-04-11 15:59:31 -0500

    Burgled House events, needs event flag from rocket battle

--- maps/CeruleanBurgledHouse.asm
+++ maps/CeruleanBurgledHouse.asm
@@ -12,6 +12,44 @@ CeruleanBurgledHouse_MapScriptHeader:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 2 ; object events
+	object_event  4,  1, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, PERSONTYPE_SCRIPT, 0, CeruleanHouseTrashedText1, -1
+	object_event  5,  6, SPRITE_GIRL, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, CeruleanHouseTrashedText2, -1
 
-	const_def 1 ; object constants
+CeruleanHouseTrashedText1:
+	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1 ; TODO: EVENT_BEAT_CERULEAN_CITY_ROCKET
+	iftrue_jumptextfaceplayer CeruleanTrashedText1_2
+	faceplayer
+	thistext
+
+CeruleanTrashedText1_1:
+	text "Those miserable"
+	line "Rockets!"
+
+	para "Look what they"
+	line "did here!"
+
+	para "They stole a TM"
+	line "for teaching"
+	cont "#mon how to"
+	cont "Dig holes!"
+
+	para "That cost me a"
+	line "bundle, it did!"
+	done
+
+CeruleanTrashedText1_2:
+	text "I figure what's"
+	line "lost is lost!"
+
+	para "I decided to teach"
+	line "Diglett how to"
+	cont "Dig without a TM!"
+	done
+
+CeruleanHouseTrashedText2:
+	text "Team Rocket must"
+	line "be trying to Dig"
+	cont "their way into no"
+	cont "good!"
+	done
