3d73fbd9795d2c9775997a84ae655b6694bcdd57
DarioMelo <dario.e.melo.d@gmail.com>
2019-09-22 18:42:37 -0300

    Added music labels

--- audio/music_pointers.asm
+++ audio/music_pointers.asm
@@ -1,63 +1,63 @@
 Music:
-	dba Music_Nothing
-	dba Music_IntroBattle
-	dba Music_RBYTitleScreen
-	dba Music_RBYPalletTown
-	dba Music_MeetProfOak
-	dba Music_OaksLab
-	dba Music_MeetRival
-	dba Music_FarewellRival
-	dba Music_Routes1
-	dba Music_WildBattle
-	dba Music_DefeatedWildMon
-	dba Music_SuccessfulCapture
-	dba Music_Cities1
-	dba Music_Pokecenter
-	dba Music_PkmnHealed
-	dba Music_Dungeon2
-	dba Music_MuseumGuy
-	dba Music_MeetFemaleTrainer
-	dba Music_TrainerBattle
-	dba Music_DefeatedTrainer
-	dba Music_Dungeon3
-	dba Music_MeetJessieJames
-	dba Music_MtMoonSquare
-	dba Music_Routes3
-	dba Music_Cities2
-	dba Music_RBYGym
-	dba Music_Routes2
-	dba Music_JigglypuffSong
-	dba Music_Vermilion
-	dba Music_SSAnne
-	dba Music_Routes4
-	dba Music_TrickHouseRSE
-	dba Music_PokeFluteChannel
-	dba Music_MeetMaleTrainer
-	dba Music_GymLeaderBattle
-	dba Music_DefeatedGymLeader
-	dba Music_BikeRiding
-	dba Music_Lavender
-	dba Music_PokemonTower
-	dba Music_Celadon
-	dba Music_CanalaveCityDPPt
-	dba Music_RBYGameCorner
-	dba Music_MeetEvilTrainer
-	dba Music_RocketBattle
-	dba Music_Dungeon1
-	dba Music_SilphCo
-	dba Music_Surfing
-	dba Music_Cinnabar
-	dba Music_CinnabarMansion
-	dba Music_SafariZone
-	dba Music_WildBattleGo
-	dba Music_SurfingPikachu
-	dba Music_RBYIndigoPlateau
-	dba Music_FinalBattle
-	dba Music_RBYHallOfFame
-	dba Music_RBYCredits
-	dba Music_BattleTowerTheme
-	dba Music_BattleTowerLobby
-	dba Music_TrainerBattleBW
+	dba Music_Nothing                    ; None
+	dba Music_IntroBattle                ; Opening Demo
+	dba Music_RBYTitleScreen             ; Title Screen
+	dba Music_RBYPalletTown              ; Pallet Town / Hall of Fame
+	dba Music_MeetProfOak                ; Prof Oak Encounter
+	dba Music_OaksLab                    ; Prof Oak's Lab
+	dba Music_MeetRival                  ; Rival Encounter
+	dba Music_FarewellRival              ; Rival Leaving
+	dba Music_Routes1                    ; Routes 1,2
+	dba Music_WildBattle                 ; Battle - Wild Pkmn
+	dba Music_DefeatedWildMon            ; Victory - Wild Pkmn
+	dba Music_SuccessfulCapture          ; Victory - Pkmn Caught
+	dba Music_Cities1                    ; Viridian City / Pewter City / Saffron City
+	dba Music_Pokecenter                 ; Pokemon Center
+	dba Music_PkmnHealed                 ; Healing
+	dba Music_Dungeon2                   ; Viridian Forest / Digglett's Cave / Seafoam Islands
+	dba Music_MuseumGuy                  ; Guide
+	dba Music_MeetFemaleTrainer          ; Female Trainer Encounter
+	dba Music_TrainerBattle              ; Battle - Trainer
+	dba Music_DefeatedTrainer            ; Victory - Trainer
+	dba Music_Dungeon3                   ; Mt. Moon / Rock Tunnel / Victory Road
+	dba Music_MeetJessieJames            ; Jessie & James Encounter
+	dba Music_MtMoonSquare               ; Mt. Moon Square
+	dba Music_Routes3                    ; Routes 3,4,5,6,7,8,9,10,16,17,18,19,20,21,22
+	dba Music_Cities2                    ; Cerulean City / Fuchsia City
+	dba Music_RBYGym                     ; Pokemon Gym
+	dba Music_Routes2                    ; Routes 24,25 / Introduction / Link Menu
+	dba Music_JigglypuffSong             ; Jigglypuff's Song
+	dba Music_Vermilion                  ; Vermilion City
+	dba Music_SSAnne                     ; S.S. Anne
+	dba Music_Routes4                    ; Routes 11,12,13,14,15
+	dba Music_TrickHouseRSE              ; Psychic Inver's House
+	dba Music_PokeFluteChannel           ; PokeFlute
+	dba Music_MeetMaleTrainer            ; Male Trainer Encounter
+	dba Music_GymLeaderBattle            ; Battle - Gym Leader / Lance
+	dba Music_DefeatedGymLeader          ; Victory - Gym Leader / Lance
+	dba Music_BikeRiding                 ; Bicycle
+	dba Music_Lavender                   ; Lavender Town
+	dba Music_PokemonTower               ; Pokemon Tower
+	dba Music_Celadon                    ; Celadon City
+	dba Music_CanalaveCityDPPt           ; Celadon University
+	dba Music_RBYGameCorner              ; Game Corner
+	dba Music_MeetEvilTrainer            ; Evil Trainer Encounter
+	dba Music_RocketBattle               ; Battle - Rocket
+	dba Music_Dungeon1                   ; Rocket Hideout / Power Plant / Cerulean Cave
+	dba Music_SilphCo                    ; Silph Co
+	dba Music_Surfing                    ; Surfing
+	dba Music_Cinnabar                   ; Cinnabar Island
+	dba Music_CinnabarMansion            ; Pokemon Mansion
+	dba Music_SafariZone                 ; Safari Zone / Evolution / Trading
+	dba Music_WildBattleGo               ; Battle - Safari Pkmn
+	dba Music_SurfingPikachu             ; Surfing Pikachu
+	dba Music_RBYIndigoPlateau           ; Indigo Plateau
+	dba Music_FinalBattle                ; Battle - Champion Rival
+	dba Music_RBYHallOfFame              ; Hall of Fame Entry
+	dba Music_RBYCredits                 ; Staff Roll
+	dba Music_BattleTowerTheme           ; Battle Tower Theme
+	dba Music_BattleTowerLobby           ; Battle Tower Lobby
+	dba Music_TrainerBattleBW            ; Battle - Battle Tower
 	dba Music_FrontierBrainBattleRSE
 	dba Music_NewBarkTown
 	dba Music_ElmsLab
@@ -93,35 +93,35 @@ Music:
 	dba Music_CianwoodCityHGSS
 	dba Music_Route47HGSS
 	dba Music_SafariZoneGateHGSS
-	dba Music_LakeOfRage
+	dba Music_LakeOfRage                 ; Navel Rock
 	dba Music_LookPokemaniac
 	dba Music_DarkCave
 	dba Music_DragonsDen
 	dba Music_TinTower
-	dba Music_Route26
+	dba Music_Route26                    ; Cerise Island
 	dba Music_ScaryHouseXY
 	dba Music_PowerPlantXY
 	dba Music_EternaForestDPPt
 	dba Music_MountChimneyRSE
 	dba Music_MountPyreRSE
-	dba Music_Route205DPPt
+	dba Music_Route205DPPt               ; Beautiful Beach
 	dba Music_WildBattleSM
-	dba Music_SunyshoreCityDPPt
+	dba Music_SunyshoreCityDPPt          ; Shamouti Island
 	dba Music_TrainerBattleSM
-	dba Music_Route203DPPt
-	dba Music_WhiteTreehollowWhite2
-	dba Music_LaverreCityXY
+	dba Music_Route203DPPt               ; Rocky Beach
+	dba Music_WhiteTreehollowWhite2      ; Noisy Forest
+	dba Music_LaverreCityXY              ; Shamouti Shrine
 	dba Music_Route101RSE
 	dba Music_EverGrandeCityRSE
-	dba Music_OreburghGateDPPt
-	dba Music_Route12BW
-	dba Music_RoadToReversalMountainB2W2
+	dba Music_OreburghGateDPPt           ; Shamouti Tunnel
+	dba Music_Route12BW                  ; Shamouti Coast
+	dba Music_RoadToReversalMountainB2W2 ; Fire/Ice/Lightning Islands
 	dba Music_LugiasSong2000
-	dba Music_LugiaBattleHGSS
+	dba Music_LugiaBattleHGSS            ; Battle - Lugia
 	dba Music_SummoningDanceHGSS
-	dba Music_HoOhBattleHGSS
-	dba Music_MewtwoBattleStadium
-	dba Music_AbandonedShipRSE
-	dba Music_KantoLegendBattleXY
-	dba Music_SuicuneBattle
-	dba Music_GymLeaderBattleRSE
+	dba Music_HoOhBattleHGSS             ; Battle - Ho-Oh
+	dba Music_MewtwoBattleStadium        ; Battle - Mewtwo
+	dba Music_AbandonedShipRSE           ; Faraway Island
+	dba Music_KantoLegendBattleXY        ; Battle - Articuno/Zapdos/Moltres/Mew
+	dba Music_SuicuneBattle              ; Battle - Celebi
+	dba Music_GymLeaderBattleRSE         ; Battle - Flannery
